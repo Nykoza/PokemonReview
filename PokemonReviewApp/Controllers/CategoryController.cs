3bf89f1f17c61939d29d00ab50e35bf21668b959
Nykoza <nicolas.goepp@epitech.eu>
2024-01-13 00:50:25 +0100

    Add categories routes + mapping return values

--- PokemonReviewApp/Controllers/CategoryController.cs
+++ PokemonReviewApp/Controllers/CategoryController.cs
@@ -23,10 +23,38 @@ public class CategoryController: ControllerBase
     [ProducesResponseType(200, Type = typeof(IEnumerable<Category>))]
     public IActionResult GetCategories()
     {
-        var categories = (_categoryRepository.GetCategories());
+        List<CategoryDto> categories = _mapper.Map<List<CategoryDto>>(_categoryRepository.GetCategories());
 
         if (!ModelState.IsValid)
             return BadRequest(ModelState);
         return Ok(categories);
+    }
+
+    [HttpGet("{categoryId}")]
+    [ProducesResponseType(200, Type = typeof(Category))]
+    public IActionResult GetCategory(int categoryId)
+    {
+        if (!_categoryRepository.CategoryExists(categoryId))
+            return NotFound();
+
+        CategoryDto category = _mapper.Map<CategoryDto>(_categoryRepository.GetCategory(categoryId));
+
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        return Ok(category);
+    }
+    
+    [HttpGet("pokemon/{categoryId}")]
+    [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+    [ProducesResponseType(400)]
+    public IActionResult GetPokemonByCategoryId(int categoryId)
+    {
+        List<PokemonDto> pokemons = _mapper.Map<List<PokemonDto>>(_categoryRepository.GetPokemonByCategory(categoryId));
+
+        if (!ModelState.IsValid)
+            return BadRequest();
+        
+        return Ok(pokemons);
     }
 }
--- PokemonReviewApp/Controllers/PokemonController.cs
+++ PokemonReviewApp/Controllers/PokemonController.cs
@@ -38,7 +38,7 @@ public class PokemonController: ControllerBase
         if (!_pokemonRepository.PokemonExists(pokeId))
             return NotFound();
         
-        var pokemon = _mapper.Map<PokemonDto>(_pokemonRepository.GetPokemon(pokeId));
+        PokemonDto pokemon = _mapper.Map<PokemonDto>(_pokemonRepository.GetPokemon(pokeId));
     
         if (!ModelState.IsValid)
             return BadRequest(ModelState);
